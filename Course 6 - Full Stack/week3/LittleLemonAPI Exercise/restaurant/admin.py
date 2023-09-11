cbaab0eb82dfcb20c1f9d1b49bd133020985db07
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2023-09-11 16:07:20 +0300

    Deleted bad code

--- Course 6 - Full Stack/week3/LittleLemonAPI Exercise/restaurant/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Menu
-from .models import Booking
-
-
-admin.site.register(Menu)
--- Course 6 - Full Stack/week3/SQL Exercise/myproject/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Booking
-
-
-# Code added for loading form data on the Booking page
-class BookingForm(ModelForm):
-    class Meta:
-        model = Booking
-        fields = "__all__"
