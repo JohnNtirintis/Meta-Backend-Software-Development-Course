76543e2e81aceedcb6c17b957e3625d5807267b8
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2023-09-11 16:08:17 +0300

    Completed the Course

--- Course 6 - Full Stack/week3/LittleLemon/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserComments
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = UserComments
-        fields = '__all__'
--- Course 6 - Full Stack/week3/LittleLemon/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('ratings', views.ratings),
-    path('blog', views.form_view ),
-]
